6ffe85a1d343b46bae06f380d98f8efbb54b0e99
mattbarich <mattbarich1@gmail.com>
2024-06-24 19:58:56 -0600

    Started Gallatin River scraping

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello World")
+import requests
+from bs4 import BeautifulSoup
+
+bfs = "https://www.bozemanflysupply.com/river-report/gallatin"
+
+
+
+
+def scrape_data():
+    response = requests.get(bfs)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    items = soup.find_all('div', class_='_1140-w-wrapper padding-top')
+    fishing_report = []
+
+    for item in items:
+        river = item.find('h1', class_='inline mobile-text-size').get_text()
+        report = item.find('div', class_='text-rich-text w-richtext').get_text()
+
+        fishing_report.append(f"{river}: {report}")
+    return '\n'.join(fishing_report)
+
+
+
+if __name__ == "__main__":
+    data = scrape_data()
+    print(data)
