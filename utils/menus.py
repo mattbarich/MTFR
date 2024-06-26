2b027adfec0cf6365c1fbf11e014402638c2de60
Matt Barich <matthew.barich@aed.pro>
2024-06-26 16:52:00 -0600

    Refactor of Menu functions.

--- utils/menus.py
+++ utils/menus.py
@@ -1,18 +1,24 @@
-def upperOrLower():
-    section = ["Upper", "lower"]
-    print("What section of the Madison?\n")
-    for inx, option in enumerate(section, start=1):
+def printMenu(options):
+    for inx, option in enumerate(options, start=1):
         print(f"{inx}. {option}")
     choice = int(input("Enter Number: "))
 
     return choice
 
+def upperOrLower():
+    section = ["Upper", "lower"]
+    print("What section of the Madison?\n")
+    choice = printMenu(section)
+
+    return choice
+
 def gallatinMenu():
-    flyShops = ["Bozeman Fly Supply", "The Rivers Edge", "Montana Angler", "Yellow Dog"]
+    flyShops = ["Bozeman Fly Supply", 
+                "The Rivers Edge", 
+                "Montana Angler", 
+                "Yellow Dog"]
     print("Which fly shop would you like your report from?\n")
-    for inx, option in enumerate(flyShops, start=1):
-        print(f"{inx}. {option}")
-    choice = int(input("Enter number: "))
+    choice = printMenu(flyShops)
 
     return choice
 
@@ -24,34 +30,35 @@ def upperMadisonMenu():
                 "The Tackle Shop", 
                 "Madison River Fishing Company"]
     print("Which fly shop would you like your report from?\n")
-    for inx, option in enumerate(flyShops, start=1):
-        print(f"{inx}. {option}")
-    choice = int(input("Enter number: "))
+    choice = printMenu(flyShops)
 
     return choice
 def lowerMadisonMenu():
-    flyShops = ["Bozeman Fly Supply", "The Rivers Edge", "Montana Angler", "Yellow Dog"]
+    flyShops = ["Bozeman Fly Supply", 
+                "The Rivers Edge", 
+                "Montana Angler", 
+                "Yellow Dog"]
     print("Which fly shop would you like your report from?\n")
-    for inx, option in enumerate(flyShops, start=1):
-        print(f"{inx}. {option}")
-    choice = int(input("Enter number: "))
+    choice = printMenu(flyShops)
 
     return choice
 
 def yellowstoneMenu():
-    flyShops = ["Bozeman Fly Supply", "The Rivers Edge", "Montana Angler", "Yellow Dog"]
+    flyShops = ["Bozeman Fly Supply", 
+                "The Rivers Edge", 
+                "Montana Angler", 
+                "Yellow Dog"]
     print("Which fly shop would you like your report from?\n")
-    for inx, option in enumerate(flyShops, start=1):
-        print(f"{inx}. {option}")
-    choice = int(input("Enter number: "))
+    choice = printMenu(flyShops)
 
     return choice
 
 def missouriMenu():
-    flyShops = ["Bozeman Fly Supply", "The Rivers Edge", "Montana Angler", "Yellow Dog"]
+    flyShops = ["Bozeman Fly Supply", 
+                "The Rivers Edge", 
+                "Montana Angler", 
+                "Yellow Dog"]
     print("Which fly shop would you like your report from?\n")
-    for inx, option in enumerate(flyShops, start=1):
-        print(f"{inx}. {option}")
-    choice = int(input("Enter number: "))
+    choice = printMenu(flyShops)
 
     return choice
